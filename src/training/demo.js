22ccc29f838391fcf8cb17d247f76eace60ebf1a
Tin Ta <trungtin0893@gmail.com>
2019-10-23 06:53:53 +0700

    training

--- src/training/demo.js
+++ src/training/demo.js
@@ -1,2 +1,14 @@
 import {createStore} from 'redux';
-const store = createStore();
+var initialState = {
+    status :false,
+    sort :{
+        by :'name',
+        value :1
+    }
+}
+var myReducer =(state = initialState, action) =>{
+    return  state;
+}
+const store = createStore(myReducer);
+console.log("default:",store);
+
